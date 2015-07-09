7ef057c31e1fa98930d91b428e17053b6835f6bf
Helbert Rico <jhelbert@gmail.com>
2015-07-09 02:22:44 -0500

    PID Listener for stop server

--- backend/pet-care/src/main/java/com/twp/petcare/app/Application.java
+++ backend/pet-care/src/main/java/com/twp/petcare/app/Application.java
@@ -1,6 +1,7 @@
 package com.twp.petcare.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,6 +12,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("/tmp/petcare.pid"));
+        springApplication.run(args);
     }
 }
