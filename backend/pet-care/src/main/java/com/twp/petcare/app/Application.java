964518b6c1737c0b00db796a4c2a9088370a6fa3
Helbert Rico <jhelbert@gmail.com>
2015-07-09 11:48:59 -0500

    Default PID

--- backend/pet-care/src/main/java/com/twp/petcare/app/Application.java
+++ backend/pet-care/src/main/java/com/twp/petcare/app/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.addListeners(new ApplicationPidFileWriter("/tmp/petcare.pid"));
+        springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
     }
 }
